d427ae1331b0ae4d4c3fa8b9ae2e0c260f7048b8
Andrew882024 <acz882024@gmail.com>
2025-09-06 21:55:13 -0700

    save

--- app/UploadEvent_page/page.tsx
+++ app/UploadEvent_page/page.tsx
@@ -9,6 +9,10 @@ import 'react-clock/dist/Clock.css';
 import { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+//import { useState } from 'react';
+
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -41,7 +45,7 @@ const UploadEvent_page = () =>{
       }
   
   
-      const handleSubmit = async () => {
+      const handleSubmitDate = async () => {
       if (!isValidDate) {
         console.warn("date is empty or wrong, try again");
         return;
@@ -74,7 +78,65 @@ const UploadEvent_page = () =>{
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                       Event_info_picker                                          
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    const [files, setFiles] = useState<(File & { preview: string })[]>([]);
+    const [rejected, setRejected] = useState<import('react-dropzone').FileRejection[]>([])
+    const removeFile = (name:string) => {
+    setFiles(files => files.filter(file => file.name !== name));
+    };
+    const onDrop = useCallback((acceptedFiles: File[], rejectedFiles: import('react-dropzone').FileRejection[]) => {
+      
+      if (rejectedFiles?.length) {
+        setRejected(previousFiles => [...rejectedFiles]);
+        alert(rejectedFiles[0].errors[0].message);
+      }
+      console.log(acceptedFiles);
+      if (acceptedFiles.length) {
+        setFiles(previousFiles => [
+          // ...previousFiles,
+          ...acceptedFiles.map(file =>
+            Object.assign(file, { preview: URL.createObjectURL(file) })
+          )
+        ])}
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop, 
+      accept: {'image/*': []},
+      maxSize: 1048576*5,
+      maxFiles:1
+  
+  });// 5MB
+  
+  const handleImageSubmit = async (e?: React.FormEvent<HTMLFormElement>) => {
+    e?.preventDefault();
+    if (!files?.length) return;
+  
+    const res = await fetch("http://localhost:8000/upload-url", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ filename: files[0].name, content_type: files[0].type }),
+    });
+  
+    const { url, fields } = await res.json();
+  
+    const formData = new FormData();
+    //files.forEach(file => formData.append('file', file));
+    const file = files[0];
+    Object.entries(fields).forEach(([k, v]) => formData.append(k, v as string)); // adds 'key'
+    formData.append("file", file);
+  
+    formData.append('upload_preset', 'friendsbook');
+  
+    const cloudinaryUrl = url //"http://localhost:8000/upload"//process.env.NEXT_PUBLIC_CLOUDINARY_URL;
+    if (!cloudinaryUrl) {
+      alert('Cloudinary URL is not defined.');
+      return;
+    }
+    const data = await fetch(cloudinaryUrl, {
+      method: 'POST',
+      body: formData
+     })//.then(res => res.json());
+  
+    console.log(data);
+  };
 
 
 
@@ -89,7 +151,29 @@ const UploadEvent_page = () =>{
       <div className="flex justify-center">
       <div className="w-[800px] bg-gray-200 rounded-[20px] min-h-[800px] box-border shadow-lg">
         <div className='text-[30px] text-blue-600 mt-[20px] mb-[5px] ml-[30px]'>Upload image</div>
-        <div className="ml-[30px]"><MyDropzone/></div>
+        <div className="ml-[30px]">
+          
+          <form onSubmit={handleImageSubmit}>
+            <div {...getRootProps({className:"w-[740px] h-[180px] border-[2px] border-dashed border-gray-300 bg-gray-100 flex flex-col items-center justify-center cursor-pointer shadow-lg rounded-[10px]"})}>
+              <input {...getInputProps()} />
+              {
+                isDragActive ?
+                  <p className='text-[20px] text-gray-700'>Drop the files here ...</p> :
+                  <p className='text-[20px] text-gray-700'>Drag 'n' drop some files here, or click to select files</p>
+              }
+            </div>
+            {/* Preview */}
+            <ul className='text-[20px] text-gray-700'>
+              {files.map(file => (
+                <li key={file.name + file.preview}> <img src={file.preview} alt="" className='w-[100px] h-[100px] object-contain inline-block'/>{file.name} {<button className='text-[20px] text-gray-700 border-2 border-black cursor-pointer' onClick={()=>removeFile(file.name)}>remove image</button>}</li>         
+              ))}
+            </ul>
+            <button type="submit" className='border-[2px] border-black rounded-[10px] text-[18px] text-gray-700 hover:shadow-lg cursor-pointer '>Upload</button>
+            
+          </form>
+    </div>
+
+
         <div className="text-[20px] text-gray-900 font-Nunito mt-[0px] mb-[5px] ml-[30px]">Event Date and Time:</div>
         <div className="ml-[30px] ">
           <div>
@@ -98,9 +182,15 @@ const UploadEvent_page = () =>{
                 
               </div>
         </div>
-        <button className="bg-blue-500 text-white p-2 rounded mt-2" onClick={() => {
-                  handleSubmit();
-                }}>Submit</button>
+        <button
+          type="button" className="bg-blue-500 text-white p-2 rounded mt-2"
+          onClick={async () => {
+            await handleImageSubmit();  
+            await handleSubmitDate();   
+          }}
+        >
+          Submit All
+        </button>
       </div>
       </div>
     </div>
