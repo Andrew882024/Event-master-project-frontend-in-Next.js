3819575d4109f586ae2f75feaa8e7b50a6f806fd
Andrew882024 <acz882024@gmail.com>
2025-10-09 15:02:20 -0700

    env test

--- src/data/severUrl.tsx
+++ src/data/severUrl.tsx
@@ -1,2 +1,4 @@
 //export const serverUrl = "http://localhost:8000";
-export const serverUrl = "http://13.58.100.234:8000";
+//export const serverUrl = "http://13.58.100.234:8000";
+
+export const serverUrl = process.env.NEXT_PUBLIC_API_URL;
